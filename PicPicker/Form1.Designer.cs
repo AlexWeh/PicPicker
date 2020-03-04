ad233ea374c879435bd2ee847a43994a89a25cc3
AlexWeh <alex.jan.w@gmail.com>
2020-03-04 10:56:23 +0100

    Init with Image View and Marker

---
+++ PicPicker/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace PicPicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.descriptionTextBox = new System.Windows.Forms.RichTextBox();
+            this.setPosition = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.imgCounterLabel = new System.Windows.Forms.Label();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.descriptionTextBox);
+            this.groupBox1.Controls.Add(this.setPosition);
+            this.groupBox1.Location = new System.Drawing.Point(1019, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 671);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(6, 19);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(168, 144);
+            this.descriptionTextBox.TabIndex = 1;
+            this.descriptionTextBox.Text = "";
+            this.descriptionTextBox.TextChanged += new System.EventHandler(this.descriptionTextBox_TextChanged);
+            // 
+            // setPosition
+            // 
+            this.setPosition.Cursor = System.Windows.Forms.Cursors.Default;
+            this.setPosition.Location = new System.Drawing.Point(6, 169);
+            this.setPosition.Name = "setPosition";
+            this.setPosition.Size = new System.Drawing.Size(75, 23);
+            this.setPosition.TabIndex = 0;
+            this.setPosition.Text = "Set Face";
+            this.setPosition.UseVisualStyleBackColor = true;
+            this.setPosition.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.ImageKey = "(none)";
+            this.label2.Location = new System.Drawing.Point(345, 291);
+            this.label2.MaximumSize = new System.Drawing.Size(64, 64);
+            this.label2.MinimumSize = new System.Drawing.Size(64, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 64);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1211, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Click here to open pictures ...";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Multiselect = true;
+            // 
+            // imgCounterLabel
+            // 
+            this.imgCounterLabel.AutoSize = true;
+            this.imgCounterLabel.Location = new System.Drawing.Point(902, 680);
+            this.imgCounterLabel.Name = "imgCounterLabel";
+            this.imgCounterLabel.Size = new System.Drawing.Size(30, 13);
+            this.imgCounterLabel.TabIndex = 4;
+            this.imgCounterLabel.Text = "(0/0)";
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(938, 675);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 5;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // prevButton
+            // 
+            this.prevButton.Location = new System.Drawing.Point(821, 675);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(75, 23);
+            this.prevButton.TabIndex = 6;
+            this.prevButton.Text = "Previous";
+            this.prevButton.UseVisualStyleBackColor = true;
+            this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1001, 642);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 710);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.prevButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.imgCounterLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PicPicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.Button setPosition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label imgCounterLabel;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.RichTextBox descriptionTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PicPicker/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicPicker
+{
+    public partial class Form1 : Form
+    {
+        private List<String> imageFileList = new List<String>();
+        private int picCounter = 0;
+        private Image currentImage;
+        private System.Text.ASCIIEncoding enc = new ASCIIEncoding();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image == null)
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string file in openFileDialog.FileNames)
+                    {
+                        try
+                        {
+                            Image tryLoadingAsImage = Image.FromFile(file);
+                            imageFileList.Add(file);
+                        }
+                        catch (Exception ex)
+                        {
+                            // Could not load the image - probably related to Windows file system permissions.
+                            MessageBox.Show("Cannot display the image: " + file.Substring(file.LastIndexOf('\\'))
+                                + ". You may not have permission to read the file, or " +
+                                "it may be corrupt.\n\nReported error: " + ex.Message);
+                        }
+                    }
+                    label1.Visible = false;
+                    
+                    loadPicture();
+                }
+            }
+            else
+            {
+                Console.WriteLine(PointToClient(Cursor.Position));
+                label2.Size = new Size(64, 64);
+                Bitmap marker = new Bitmap(Image.FromFile("Untitled.bmp"));
+                marker.MakeTransparent(marker.GetPixel(10,10));
+                label2.Parent = pictureBox;
+                label2.BackColor = Color.Transparent;
+                label2.Image = marker;
+                label2.Location = PointToClient(new Point(Cursor.Position.X - 45, Cursor.Position.Y - 60));
+            }
+        }
+
+        private void loadPicture()
+        {
+            if (picCounter >= 0 || picCounter < imageFileList.Count)
+            {
+                currentImage = Image.FromFile(imageFileList[picCounter]);
+                pictureBox.Image = currentImage;
+                //Console.WriteLine(currentImage.GetPropertyItem(271).Type);
+                
+                try
+                {
+                    hasDescriptionMeta(currentImage.GetPropertyItem(270));
+                }
+                catch (Exception)
+                {
+                    noDescriptionMeta(currentImage.GetPropertyItem(40092));
+                }
+                imgCounterLabel.Text = "(" + (picCounter + 1) + "/" + imageFileList.Count + ")";
+            }
+        }
+
+        private void noDescriptionMeta(PropertyItem propertyItem)
+        {
+            string result = "";
+            string temp = enc.GetString(propertyItem.Value);
+            for (int i = 0; i < temp.Length; i += 2)
+            {
+                result += temp[i];
+            }
+            this.descriptionTextBox.Text = result;
+            //propertyItem.Id = 270;
+            //propertyItem.Type = 2;
+            //currentImage.SetPropertyItem(propertyItem);
+        }
+
+        private void hasDescriptionMeta(PropertyItem propertyItem)
+        {
+            descriptionTextBox.Text = enc.GetString(propertyItem.Value); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            picCounter++;
+            loadPicture();
+        }
+
+        private void prevButton_Click(object sender, EventArgs e)
+        {
+            picCounter--;
+            loadPicture();
+        }
+
+        private void descriptionTextBox_TextChanged(object sender, EventArgs e)
+        {
+            PropertyItem propertyItem = currentImage.GetPropertyItem(40092);
+            Console.WriteLine(descriptionTextBox.Text);
+            propertyItem.Value = enc.GetBytes(descriptionTextBox.Text);
+            currentImage.SetPropertyItem(propertyItem);
+        }
+    }
+}
